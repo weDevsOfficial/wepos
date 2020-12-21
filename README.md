65048440283c0d93bfb466b2ed8391c3a3a358ee
Nurul Umbhiya <zikubd@gmail.com>
2020-12-21 19:20:44 +0600

    Bumped version to 1.1.4

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 **Donate Link:** http://tareq.co/donate/
 **Tags:** WooCommerce POS, point of sale, free pos, pos plugin, woocommerce point of sale
 **Requires at least:** 4.4
-**Tested up to:** 5.3.2
+**Tested up to:** 5.6
 **WC requires at least:** 3.0
-**WC tested up to:** 3.8.1
+**WC tested up to:** 4.8
 **Requires PHP:** 5.6
-**Stable tag:** trunk
+**Stable tag:** 1.1.4
 **License:** GPLv2 or later
 **License URI:** http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -59,6 +59,22 @@ No FAQ
 9. Admin Dashboard Settings
 
 ## Changelog ##
+
+### v1.1.4 -> 21 December, 2020 ###
+
+- **Enhancement:** Added limited promotion banner
+
+### v1.1.3 -> 23 November, 2020 ###
+
+- **Enhancement:** Added limited promotion banner
+
+### v1.1.2 -> 28 October, 2020 ###
+- **Fix:** Permission callback warnings
+- **Fix:** Duplicate order get generated if pay now button pressed twice while doing payment.
+- **Fix:** While Dokan installed, login was redirecting back to account page.
+- **Feature:** Cash input checking and Validation for Cash payment
+- **Feature:** Cart data validation for payment for currently active cart tab
+- **Feature:** Dynamic Pay now button based on current cart tab
 
 ### v1.1.1 -> 23 December, 2019 ###
 - **Tweak**  Appser client updated
---
+++ bin/zip.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# Exit if any command fails.
+set -e
+
+# Change to the expected directory.
+cd "$(dirname "$0")"
+cd ..
+DIR=$(pwd)
+BUILD_DIR="$DIR/build/wepos"
+
+# Enable nicer messaging for build status.
+BLUE_BOLD='\033[1;34m';
+GREEN_BOLD='\033[1;32m';
+RED_BOLD='\033[1;31m';
+YELLOW_BOLD='\033[1;33m';
+COLOR_RESET='\033[0m';
+error () {
+    echo -e "\n${RED_BOLD}$1${COLOR_RESET}\n"
+}
+status () {
+    echo -e "\n${BLUE_BOLD}$1${COLOR_RESET}\n"
+}
+success () {
+    echo -e "\n${GREEN_BOLD}$1${COLOR_RESET}\n"
+}
+warning () {
+    echo -e "\n${YELLOW_BOLD}$1${COLOR_RESET}\n"
+}
+
+status "💃 Time to build the wePOS Pro ZIP file 🕺"
+
+# remove the build directory if exists and create one
+rm -rf "$DIR/build"
+mkdir -p "$BUILD_DIR"
+
+# Run the build.
+# status "Installing dependencies... 📦"
+# npm install
+
+status "Generating build... 👷‍♀️"
+npm run dev-build
+npm run build
+
+# Copy all files
+status "Copying files... ✌️"
+FILES=(wepos.php readme.txt assets includes languages templates composer.json composer.lock)
+
+for file in ${FILES[@]}; do
+  cp -R $file $BUILD_DIR
+done
+
+# Delete the source JS directory
+rm -rf "$BUILD_DIR/assets/src"
+rm -rf "$BUILD_DIR/assets/less"
+
+# Install composer dependencies
+status "Installing dependencies... 📦"
+cd $BUILD_DIR
+composer install --optimize-autoloader --no-dev -q
+
+# Remove composer files
+rm composer.json composer.lock
+
+# go one up, to the build dir
+status "Creating archive... 🎁"
+cd ..
+zip -r -q wepos.zip wepos
+
+# remove the source directory
+rm -rf wepos
+
+success "Done. You've built wePOS! 🎉 "

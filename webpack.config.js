58fd0fe3c8bb82327b672ad3aaeeceea1e0fa090
Asad Nur <asaduzzamannur.dev@gmail.com>
2022-08-03 17:30:40 +0600

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const webpack = require('webpack');
 const path = require('path');
 const package = require('./package.json');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
-const BrowserSyncPlugin = require( 'browser-sync-webpack-plugin' );
-
-const config = require( './config.json' );
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { VueLoaderPlugin } = require('vue-loader')
 
 const vendorPackages = Object.keys(package.dependencies);
 vendorPackages.splice(vendorPackages.indexOf('lodash'), 1);
@@ -32,56 +31,22 @@ function isProduction() {
     return process.env.WEBPACK_ENV === 'production';
 }
 
-// extract css into its own file
-const extractCss = new ExtractTextPlugin({
-    filename: "../css/[name].css",
-});
-
-plugins.push( extractCss );
-
-// Extract all 3rd party modules into a separate 'vendor' chunk
-plugins.push(new webpack.optimize.CommonsChunkPlugin({
-    name: 'vendor',
-    minChunks: ({ resource }) => /node_modules/.test(resource),
-}));
-
-// plugins.push(new BrowserSyncPlugin( {
-//     proxy: {
-//         target: config.proxyURL
-//     },
-//     files: [
-//         '**/*.php'
-//     ],
-//     cors: true,
-//     reloadDelay: 0
-// } ));
-
-// Generate a 'manifest' chunk to be inlined in the HTML template
-// plugins.push(new webpack.optimize.CommonsChunkPlugin('manifest'));
-
-// Compress extracted CSS. We are using this plugin so that possible
-// duplicated CSS from different components can be deduped.
-plugins.push(new OptimizeCSSPlugin({
-    cssProcessorOptions: {
-        safe: true,
-        map: {
-            inline: false
-        }
+plugins.push( new MiniCssExtractPlugin(
+    {
+        filename: ( { chunk } ) => {
+            return '../css/[name].css';
+        },
     }
-}));
+) );
+plugins.push( new VueLoaderPlugin() );
 
 // Differ settings based on production flag
 if ( isProduction() ) {
-
-    plugins.push(new UglifyJsPlugin({
-        sourceMap: true,
-    }));
-
-    plugins.push(new webpack.DefinePlugin({
-        'process.env': {
-            NODE_ENV: JSON.stringify(env)
-        }
-    }));
+    // plugins.push(new webpack.DefinePlugin({
+    //     'process.env': {
+    //         NODE_ENV: JSON.stringify(env)
+    //     }
+    // }));
 
     appName = '[name].min.js';
 } else {
@@ -98,7 +63,7 @@ module.exports = {
         path: exportPath,
         filename: appName,
         chunkFilename: 'chunks/[chunkhash].js',
-        jsonpFunction: 'pluginWebpack'
+        // jsonpFunction: 'pluginWebpack'
     },
 
     resolve: {
@@ -108,10 +73,10 @@ module.exports = {
             'frontend': path.resolve('./assets/src/frontend/'),
             'admin': path.resolve('./assets/src/admin/'),
         },
-        modules: [
-            path.resolve('./node_modules'),
-            path.resolve(path.join(__dirname, 'assets/src/')),
-        ]
+        // modules: [
+        //     path.resolve('./node_modules'),
+        //     path.resolve(path.join(__dirname, 'assets/src/')),
+        // ]
     },
 
     externals: {
@@ -123,11 +88,10 @@ module.exports = {
     rules: [
         {
             test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015']
-            }
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            },
         },
         {
             test: /\.vue$/,
@@ -138,17 +102,19 @@ module.exports = {
         },
         {
             test: /\.less$/,
-            use: extractCss.extract({
-                use: [{
-                    loader: "css-loader"
-                }, {
-                    loader: "less-loader"
-                }]
-            })
+            use: [
+                // compiles Less to CSS
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                "less-loader",
+            ],
         },
         {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
+            test: /\.css$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader"
+            ],
         },
         {
             test: /\.(png|woff|woff2|eot|ttf|svg)$/,
@@ -162,5 +128,12 @@ module.exports = {
             ],
         }
     ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin()
+        ],
     },
 }

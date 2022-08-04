9f2c899c81a783887cbc0de769f3e329c01051a7
Asad Nur <asaduzzamannur.dev@gmail.com>
2022-08-04 12:32:52 +0600

    Refactored webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 const package = require('./package.json');
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
@@ -11,7 +10,6 @@ const vendorPackages = Object.keys(package.dependencies);
 vendorPackages.splice(vendorPackages.indexOf('lodash'), 1);
 
 // Naming and path settings
-var appName = 'app';
 var entryPoint = {
     frontend: './assets/src/frontend/main.js',
     admin: './assets/src/admin/main.js',
@@ -23,117 +21,101 @@ var entryPoint = {
 
 var exportPath = path.resolve(__dirname, './assets/js');
 
-// Enviroment flag
-var plugins = [];
-var env = process.env.WEBPACK_ENV;
+module.exports = ( env, argv ) => {
+    let appName = argv.mode === 'development' ? '[name].js' : '[name].min.js';
+    let appNameCss = argv.mode === 'development' ? '../css/[name].css' : '../css/[name].min.css';
 
-function isProduction() {
-    return process.env.WEBPACK_ENV === 'production';
-}
+    return {
+        entry: entryPoint,
+        output: {
+            path: exportPath,
+            filename: appName,
+            chunkFilename: 'chunks/[chunkhash].js',
+        },
 
-plugins.push( new MiniCssExtractPlugin(
-    {
-        filename: ( { chunk } ) => {
-            return '../css/[name].css';
+        resolve: {
+            alias: {
+                'vue$': 'vue/dist/vue.esm.js',
+                '@': path.resolve('./assets/src/'),
+                'frontend': path.resolve('./assets/src/frontend/'),
+                'admin': path.resolve('./assets/src/admin/'),
+            },
+            modules: [
+                path.resolve('./node_modules'),
+                path.resolve(path.join(__dirname, 'assets/src/')),
+            ]
+        },
+
+        externals: {
+            _: 'window.wepos._'
+        },
+
+        plugins: [
+            new MiniCssExtractPlugin(
+                {
+                    filename: ( { chunk } ) => {
+                        return appNameCss;
+                    },
+                }
+            ),
+            new VueLoaderPlugin(),
+            new webpack.ProvidePlugin({
+                _: '_'
+            })
+        ],
+        module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    extractCSS: true
+                }
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    // compiles Less to CSS
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "less-loader",
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ],
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'fonts',
+                    },
+                  },
+                ],
+            }
+        ]
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: false
+                }),
+                new CssMinimizerPlugin()
+            ],
         },
     }
-) );
-plugins.push( new VueLoaderPlugin() );
-
-// Differ settings based on production flag
-if ( isProduction() ) {
-    // plugins.push(new webpack.DefinePlugin({
-    //     'process.env': {
-    //         NODE_ENV: JSON.stringify(env)
-    //     }
-    // }));
-
-    appName = '[name].min.js';
-} else {
-    appName = '[name].js';
-}
-
-plugins.push(new webpack.ProvidePlugin({
-    _: '_'
-}));
-
-module.exports = {
-    entry: entryPoint,
-    output: {
-        path: exportPath,
-        filename: appName,
-        chunkFilename: 'chunks/[chunkhash].js',
-        // jsonpFunction: 'pluginWebpack'
-    },
-
-    resolve: {
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js',
-            '@': path.resolve('./assets/src/'),
-            'frontend': path.resolve('./assets/src/frontend/'),
-            'admin': path.resolve('./assets/src/admin/'),
-        },
-        // modules: [
-        //     path.resolve('./node_modules'),
-        //     path.resolve(path.join(__dirname, 'assets/src/')),
-        // ]
-    },
-
-    externals: {
-        _: 'window.wepos._'
-    },
-
-    plugins,
-    module: {
-    rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-            },
-        },
-        {
-            test: /\.vue$/,
-            loader: 'vue-loader',
-            options: {
-                extractCSS: true
-            }
-        },
-        {
-            test: /\.less$/,
-            use: [
-                // compiles Less to CSS
-                MiniCssExtractPlugin.loader,
-                "css-loader",
-                "less-loader",
-            ],
-        },
-        {
-            test: /\.css$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                "css-loader"
-            ],
-        },
-        {
-            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'fonts',
-                },
-              },
-            ],
-        }
-    ]
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin(),
-            new CssMinimizerPlugin()
-        ],
-    },
 }

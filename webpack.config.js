4cc6e15a3042306da22b58522f82e94b914d6813
Asad Nur <asaduzzamannur.dev@gmail.com>
2022-08-10 09:44:14 +0600

    Fixed indentation and removed some unused configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const webpack = require('webpack');
 const path = require('path');
 const package = require('./package.json');
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const { VueLoaderPlugin } = require('vue-loader')
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 
 const vendorPackages = Object.keys(package.dependencies);
 vendorPackages.splice(vendorPackages.indexOf('lodash'), 1);
@@ -27,25 +27,24 @@ module.exports = ( env, argv ) => {
 
     return {
         entry: entryPoint,
+
         output: {
             path: exportPath,
             filename: appName,
             chunkFilename: 'chunks/[chunkhash].js',
         },
+
         // performance: {
         //     hints: false,
         // },
+
         resolve: {
             alias: {
                 'vue$': 'vue/dist/vue.esm.js',
                 '@': path.resolve('./assets/src/'),
                 'frontend': path.resolve('./assets/src/frontend/'),
                 'admin': path.resolve('./assets/src/admin/'),
-            },
-            modules: [
-                path.resolve('./node_modules'),
-                path.resolve(path.join(__dirname, 'assets/src/')),
-            ]
+            }
         },
 
         externals: {
@@ -65,51 +64,53 @@ module.exports = ( env, argv ) => {
                 _: '_'
             })
         ],
+
         module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    extractCSS: true
-                }
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    // compiles Less to CSS
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "less-loader",
-                ],
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader"
-                ],
-            },
-            {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                        outputPath: 'fonts',
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
                     },
-                  },
-                ],
-            }
-        ]
+                },
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                    options: {
+                        extractCSS: true
+                    }
+                },
+                {
+                    test: /\.less$/,
+                    use: [
+                        // compiles Less to CSS
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "less-loader",
+                    ],
+                },
+                {
+                    test: /\.css$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader"
+                    ],
+                },
+                {
+                    test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                    use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts',
+                        },
+                    },
+                    ],
+                }
+            ]
         },
+
         optimization: {
             minimize: true,
             minimizer: [

ef6d01c42b84d2263024152f1dc21525976e1b91
Asad Nur <asaduzzamannur.dev@gmail.com>
2023-05-10 16:28:14 +0600

    update: Installer and upgrader database table creation SQL based on PR review changes

--- includes/Admin/Updates/upgrade-1.2.5.php
+++ includes/Admin/Updates/upgrade-1.2.5.php
@@ -23,19 +23,20 @@ function wepos_create_product_log_tables() {
 
     include_once ABSPATH . 'wp-admin/includes/upgrade.php';
 
-    $tables = [
+    $collate = $wpdb->get_charset_collate();
+    $tables  = [
         "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}wepos_product_logs` (
             `id` bigint unsigned NOT NULL AUTO_INCREMENT,
             `product_id` bigint unsigned NOT NULL,
-            `product_title` varchar(255) NOT NULL,
-            `product_type` varchar(255) NOT NULL,
-            `product_sku` varchar(255) NULL,
+            `product_title` text(255) NOT NULL,
+            `product_type` varchar(100) NOT NULL,
+            `product_sku` varchar(100) NULL,
             `product_price` decimal (19,
             4) NOT NULL DEFAULT 0.0000,
         `product_stock` bigint signed NULL,
         `counter_counts` bigint unsigned NULL DEFAULT 0,
         PRIMARY KEY (`id`)
-        ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;",
+        ) ENGINE=InnoDB AUTO_INCREMENT=1 {$collate};",
 
         "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}wepos_product_log_counters` (
             `id` bigint unsigned NOT NULL AUTO_INCREMENT,
@@ -43,7 +44,7 @@ function wepos_create_product_log_tables() {
             `counter_id` bigint unsigned NOT NULL,
             PRIMARY KEY (`id`),
         FOREIGN KEY (product_log_id) REFERENCES wp_wepos_product_logs (id) ON DELETE CASCADE
-        ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;",
+        ) ENGINE=InnoDB AUTO_INCREMENT=1 {$collate};",
     ];
 
     // Include 'upgrade.php' if not included earlier.
